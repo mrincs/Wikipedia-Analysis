b22de35e44c23dd7a972b609c733d4676804de5d
Mrinmoy Maity <mmaity@indiana.edu>
2015-03-31 16:37:30 -0400

    Structural Balance, Controversial pages on Simple Wiki

--- create_wiki_graph.py
+++ create_wiki_graph.py
@@ -6,7 +6,6 @@ Created on Fri Feb 20 17:30:35 2015
 """
 
 import networkx as nx
-import matplotlib.pyplot as plt
 
 def initialize_wiki_graph():
     G = nx.Graph()
@@ -23,9 +22,7 @@ def update_user_links(G,user1,user2,weight):
     if G.has_edge(user1,user2):
         G[user1][user2]['weight']+=weight
     else:
-#        print "(",user1,",",user2,") does not exist" 
         G.add_edge(user1,user2,weight=weight)       
-#    print user1,user2," ",G[user1][user2]['weight']
     return
 
 #Update user network
@@ -37,23 +34,19 @@ def include_in_network(G,reverter,revertedTo,reverted):
     update_user_links(G,reverter,revertedTo,1)
 #    update_user_links(G,reverted,revertedTo,-1)
     return
-
-def draw_wiki_graph(G):
-    nx.draw_circular(G)
-    plt.savefig("wikiusers.png")
     
-def graph_properties(G):
-    print "#######"
-    print "Nodes:"
-    print "====================="
-    print G.nodes()
-    print "Edges:"
-    print "====================="
-    print G.edges()
-    print "Radius:", nx.radius(G)
-    print "Diameter:", nx.diameter(G)
-    print "Density:", nx.density(G)
-    print "Periphery:", nx.periphery(G)
+#def graph_properties(G):
+#    print "#######"
+#    print "Nodes:"
+#    print "====================="
+#    print G.nodes()
+#    print "Edges:"
+#    print "====================="
+#    print G.edges()
+#    print "Radius:", nx.radius(G)
+#    print "Diameter:", nx.diameter(G)
+#    print "Density:", nx.density(G)
+#    print "Periphery:", nx.periphery(G)
     
 def parseRevisionFile(inputFile):
     G = initialize_wiki_graph()
@@ -64,22 +57,71 @@ def parseRevisionFile(inputFile):
             reverter = int(words[0].strip())
             revertedTo = int(words[1].strip())
             reverted = int(words[2].strip())
-            if reverter != -1 and revertedTo != -1 and reverted != -1:
-                include_in_network(G,reverter,revertedTo,reverted)
+            #if reverter != -1 and revertedTo != -1 and reverted != -1:
+            include_in_network(G,reverter,revertedTo,reverted)
 #                print reverter,revertedTo,reverted
         else:
             #New pageID
             continue
-    nx.write_gml(G,'user_graph.gml')
+#    nx.write_gml(G,'United_States.gml')
+    structural_balance(G)
 #    graph_properties(G)
-    #draw_wiki_graph(G)
+
+
+def structural_balance(G):
+
+    node_list = []
+    errors = 0
+    balanced_triplets = 0
+    unbalanced_triplets = 0
+    unbalanced_triplets_all_negatives = 0
+    unbalanced_triplets_one_negative = 0
+    
+    for node in G.nodes():
+        node_list.append(node)
+    for i in range(len(node_list)):
+        for j in range(len(node_list)):
+            for k in range(len(node_list)):
+                node_A = node_list[i]
+                node_B = node_list[j]
+                node_C = node_list[k]
+                if (i!=j) and (j!=k) and (i!=k):
+                    if G.has_edge(node_A,node_B) and G.has_edge(node_B, node_C) and G.has_edge(node_C, node_A):
+                            edge_AB = (G[node_A][node_B]['weight']>=0)
+                            edge_BC = (G[node_B][node_C]['weight']>=0)
+                            edge_CA = (G[node_C][node_A]['weight']>=0)
+                            if (edge_AB and edge_BC and edge_CA) or (edge_AB + edge_BC + edge_CA == 1):
+                                balanced_triplets += 1
+                            elif (edge_AB + edge_BC + edge_CA == 0):
+                                unbalanced_triplets += 1
+                                unbalanced_triplets_all_negatives += 1
+                            elif (edge_AB + edge_BC + edge_CA == 2):
+                                unbalanced_triplets += 1
+                                unbalanced_triplets_one_negative += 1
+                            else:
+                                errors += 1
+   
+    print "Total number of Nodes:", len(node_list)
+    print "Total number of Edges:", G.number_of_edges()
+    print "Maximum possible Edges:", ((len(node_list))*(len(node_list)-1)*(len(node_list)-2)/6)
+    print "Edge Density:", (G.number_of_edges()*6.0/((len(node_list))*(len(node_list)-1)*(len(node_list)-2)))
+    print "balanced_triplets:",balanced_triplets
+    print "unbalanced_triplets:",unbalanced_triplets
+    print "unbalanced_triplets_all_negatives:",unbalanced_triplets_all_negatives
+    print "unbalanced_triplets_one_negative:",unbalanced_triplets_one_negative
+    print "errors:",errors
+
 
 def main():
-    inputpath = "revision.log"
-    #inputpath = "test.log"
-    inputFile = open(inputpath,"r")
-    parseRevisionFile(inputFile)
-    inputFile.close()
+    files = ["Anarchism","Christianity","Circumcision","George_W._Bush","Global_warming","Jesus","LWWEe","Muhammad","United_States"]
+    for idx in range(len(files)):
+        print files[idx]
+        print "---------------"
+        inputpath = "C:\WikiProject\Controversial Pages Single\Revision Logs\\"+files[idx]+".log"
+        #inputpath = "test.log"
+        inputFile = open(inputpath,"r")
+        parseRevisionFile(inputFile)
+        inputFile.close()
     
 
 main()    
--- wiki_visualizations.py
+++ wiki_visualizations.py
@@ -12,43 +12,38 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import random
 import math
+#import os
 
-def user_correlations_histogram(input_gml):
+def user_network_visualizations(input_gml):
+#    print input_gml
     G = nx.read_gml(input_gml)
+    
     weights = []
     for source,dest,attr in G.edges(data=True):
         weights.append(attr['weight'])
-    np_weights = np.array(weights)
-#    visualize_user_network_custom_layout(G)
+    visualize_histogram(weights,input_gml)
+    visualize_user_network_custom_layout(G)
 #    visualize_user_network_networkx_layout(G)
-#    print(np_weights)
-#    unique, counts = np.unique(np_weights, return_counts=True)
-#    hist_to_plot = np.asarray((unique, counts)).T
-    draw_histogram(np_weights)
-#    plt.hist(weights, bins = 50)
 
-def draw_histogram(x):
-    num_bins=1000
-    x1, x2 = -300, 300
-    y1, y2 = 0, 70000
-    # the histogram of the data
-    n, bins, patches = plt.hist(x, num_bins, facecolor='green', alpha=1)
-    # add a 'best fit' line
+def visualize_histogram(weights,file):
+#    filename = os.path.basename(file)
+    x = np.array(weights)
+    print x
+#    x1, x2 = -5, 5
+#    y1, y2 = 0, 100
+#    num_bins = x2-x1
+    plt.hist(x, facecolor='green', bins = max(x)-min(x))
     plt.xlabel('# Agreement among Users')
     plt.ylabel('Frequencies')
     plt.title("User Agreements")
-    plt.axis([x1, x2, y1, y2])
-
-    # Tweak spacing to prevent clipping of ylabel
-    plt.subplots_adjust(left=0.15)
-    plt.savefig("user_correlations_hist.png")
+#    plt.axis([x1, x2, y1, y2])
     plt.show()
 
 
 def visualize_user_network_networkx_layout(G):
 	
-    options = ['spring','circular','spectral','shell','random']
-    
+#    options = ['spring','circular','spectral','shell','random']
+    options = ['circular']    
     pos_edges = [(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] > 0]
     neg_edges = [(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] < 0]
     neutral_edges = [(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] == 0]
@@ -64,9 +59,9 @@ def visualize_user_network_networkx_layout(G):
                    width=1,alpha=0.5,edge_color='g')
 	nx.draw_networkx_edges(G,node_pos,edgelist=neutral_edges,
                    width=1,edge_color='y')
-        op_file = "user_network_"+option+"_layout.png"
+#      op_file = "user_network_"+option+"_layout.png"
 	plt.axis('off')
-	plt.savefig(op_file) # save as png
+#	plt.savefig(op_file) # save as png
 	plt.show() # display
 	
 
@@ -86,102 +81,6 @@ def networkx_layout_options(G, option):
 def log_scaling_of_edge_weights(n):
     return math.log10(1+n)+1
     
-#def visualize_user_network_custom_layout(G):
-#    
-#    X_CENTRE = 500
-#    Y_CENTRE = 300
-#    X_UNIT = 700
-#    Y_UNIT = 1000    
-#    ZERO = 100
-#    
-#    nodes_dict = {}
-#    
-#    #Initializing colors for nodes
-#    colors_file = open("colors.txt",'r')
-#    colors = colors_file.readlines()
-#    random.seed()
-#    random.shuffle(colors)
-##    print(colors)
-#    
-#    if (len(G.nodes()) > 0):
-#        
-#        # Below algorithm is for layout structure of the graph. 
-#        # Taken from "Network Analysis of 
-#        # Collaboration Structure in Wikipedia" by Brades et. all
-#        
-#        # Calculate λ and  λ'
-#        adj_matrix = nx.to_numpy_matrix(G)
-#        
-##        for i in range(len(adj_matrix)):
-##            adj_matrix[i] = map(log_scaling_of_edge_weights, adj_matrix[i])
-#
-##        for x_idx in range(len(adj_matrix)):
-##            for y_idx in range(len(adj_matrix[x_idx])):
-###                mod_weight = math.log10(1 + adj_matrix[x_idx][y_idx]) + 1
-###                adj_matrix[x_idx][y_idx] = mod_weight
-###                adj_matrix[y_idx][x_idx] = mod_weight
-##                print x_idx, y_idx, adj_matrix[x_idx][y_idx]
-#        
-#        eigenvalues, eigenvectors = np.linalg.eig(adj_matrix)
-#        copy_eigenvalues = eigenvalues.copy()
-#        lambda_min = min(eigenvalues)
-#        copy_eigenvalues[np.argmin(eigenvalues)] = float("inf")
-#        lambda_min_prime = min(copy_eigenvalues)
-#        
-#        #Compute eigen vectors associated with  λ and  λ'
-#        x = eigenvectors[:,np.argmin(eigenvalues)]
-#        y = eigenvectors[:,np.argmin(copy_eigenvalues)]
-#
-#        if ((lambda_min<0) and (lambda_min_prime<0)):
-#            y = y*(lambda_min_prime/lambda_min)
-#
-#        i = 0        
-#        node_pos = {} #Collect the node positions
-#        
-#        for node in G.nodes():
-#            
-#            # Get the co-ordinates from eigen vectors
-#            x_i = x[i].real
-#            y_i = y[i].real
-#            
-#            # Transpolate the coordinates.
-#            x_i = min(X_CENTRE + (x_i * X_UNIT), X_CENTRE * 2)
-#            y_i = min(Y_CENTRE + (y_i * Y_UNIT), Y_CENTRE * 2)
-#            
-#            # Avoid nodes going out of the screen.
-#            x_i = max(x_i, ZERO)
-#            y_i = max(y_i, ZERO)
-#            
-#            # Assign a color to the node.
-#            if node not in nodes_dict.keys():
-#                nodes_dict.update({node : len(nodes_dict) +1 })  
-#            node_color = colors[nodes_dict[node] % len(colors)].rstrip()
-#            
-#            node_pos[node] = [x_i, y_i]        
-#            i = i+1
-#
-#
-#        
-##        for i in range(len(adj_matrix)):
-##            for j in range(i, len(adj_matrix)):
-##                weight = adj_matrix[i,j]
-#        
-#        pos_edges = [(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] > 0]
-#        neg_edges = [(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] < 0]
-#        neutral_edges = [(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] == 0]
-#        
-#        nx.draw_networkx_nodes(G, node_pos, node_size=10)
-#        nx.draw_networkx_edges(G, node_pos)
-##        nx.draw_networkx_edges(G,node_pos,edgelist=neg_edges)
-##        nx.draw_networkx_edges(G,node_pos,edgelist=neutral_edges,
-##                   width=1,alpha=0.5,edge_color='y')
-#        plt.axis('off')
-#        plt.savefig("user_network_custom_layout.png") # save as png
-#        plt.show() # display        
-#                      
-#
-#    
-#    colors_file.close()
 
 
 def visualize_user_network_custom_layout(G):
@@ -225,16 +124,18 @@ def visualize_user_network_custom_layout(G):
         lambda_min = min(eigenvalues)
         copy_eigenvalues[np.argmin(eigenvalues)] = float("inf")
         lambda_min_prime = min(copy_eigenvalues)
-        
         #Compute eigen vectors associated with  λ and  λ'
         x = eigenvectors[:,np.argmin(eigenvalues)]
         y = eigenvectors[:,np.argmin(copy_eigenvalues)]
+#        x = x.flatten()
+#        y = y.flatten()
+#        print x, y
 
         if ((lambda_min<0) and (lambda_min_prime<0)):
             y = y*(lambda_min_prime/lambda_min)
 
 
-        
+       
         # Calculate the node positions
         i = 0        
         node_pos = {} #Collect the node positions
@@ -258,11 +159,10 @@ def visualize_user_network_custom_layout(G):
                 nodes_dict.update({node : len(nodes_dict) +1 })  
             node_color = colors[nodes_dict[node] % len(colors)].rstrip()
             
-            node_pos[node] = [x_i, y_i]
+            node_pos[node] = [np.array(x_i).reshape(-1,).tolist()[0], np.array(y_i).reshape(-1,).tolist()[0]]
             node_colors.append(node_color)
             i = i+1
         
-
 
         # Calculate the node sizes based on neighbour counts/total neighbour weights
         node_sizes_neighbour_count = []
@@ -286,12 +186,6 @@ def visualize_user_network_custom_layout(G):
 #            for j in range(i, len(adj_matrix)):
 #                weight = adj_matrix[i,j]
         
-        pos_edges = [(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] > 0]
-        neg_edges = [(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] < 0]
-        neutral_edges = [(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] == 0]
-        
-        nx.draw_networkx_nodes(G, node_pos, node_size=10)
-        nx.draw_networkx_edges(G, node_pos)
         
         # Partitioning the edges
         pos_edges = [(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] > 0]
@@ -299,13 +193,10 @@ def visualize_user_network_custom_layout(G):
         neutral_edges = [(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] == 0]
         
         nx.draw_networkx_nodes(G, pos=node_pos, node_size=node_sizes_neighbour_count)
-#        nx.draw_networkx_labels(G, pos=node_pos, font_size=5)
-        nx.draw_networkx_edges(G,node_pos,
-                   width=1,alpha=0.5,edge_color='b')
-#        nx.draw_networkx_edges(G,node_pos,edgelist=neg_edges,
-#                   width=1,alpha=0.5,edge_color='g')
-#        nx.draw_networkx_edges(G,node_pos,edgelist=neutral_edges,
-#                   width=1,alpha=0.5,edge_color='y')
+#        nx.draw_networkx_labels(G, pos=node_pos, font_size=10)
+        nx.draw_networkx_edges(G,node_pos, edgelist=pos_edges,width=1,alpha=0.5,edge_color='b')
+        nx.draw_networkx_edges(G,node_pos,edgelist=neg_edges,width=1,alpha=0.5,edge_color='g')
+        nx.draw_networkx_edges(G,node_pos,edgelist=neutral_edges,width=1,alpha=0.5,edge_color='y')
         plt.axis('off')
         plt.savefig("user_network_custom_layout.png") # save as png
         plt.show() # display        
@@ -314,13 +205,19 @@ def visualize_user_network_custom_layout(G):
     
     colors_file.close()
 
-   
-    
-#user_correlations_histogram('test_user_graph.gml')
-user_correlations_histogram('user_graph.gml')
 
-#a = [[1,2,3],[4,5,6],[7,8,9]]
-#print a
-#for i in range(len(a)):
-#    for j in range(len(a[i])):
-#        print a[i][j]
+def main():
+    files = ["Anarchism","Christianity","Circumcision","George_W._Bush","Global_warming","Jesus","LWWEe","Muhammad","United_States"]
+    for idx in range(len(files)):
+        print files[idx]
+        print "---------------"
+        inputpath = "C:\WikiProject\Controversial Pages Single\User Graphs With Anonymous\\"+files[idx]+".gml"
+        inputFile = open(inputpath,"r")
+        user_network_visualizations(inputFile)
+        inputFile.close()
+
+
+main()
+#user_network_visualizations('C:\WikiProject\Controversial Pages Single\User Graphs With Anonymous\Anarchism.gml')
+#user_network_visualizations('user_graph.gml')
+#user_network_visualizations('test_user_graph.gml')
